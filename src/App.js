2f52c757dcee83c8fbc1f6bb5ea0f6d8ccf03142
David Paucar <soydavidpaucar@gmail.com>
2022-05-21 17:35:30 -0500

    :art: added handle functions and app structure
    created app structure and created handle functions to inputs and its operations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import picoYPlacaConfig from './config/pico-y-placa-configs.json';
 
 const apiKey = process.env.REACT_APP_APIKEY;
 
@@ -10,7 +11,84 @@ const handleFetchHolidays = async () => {
 };
 
 const App = () => {
-
+	// handle variable states
+	const [plateNumber, setPlateNumber] = useState('');
+	const [plateDate, setPlateDate] = useState('');
+	const [plateTime, setPlateTime] = useState('');
+	const [holidays, setHolidays] = useState({});
+	
+	// fetch holidays from google calendar
+	useEffect(() => {
+		handleFetchHolidays().then(response => {
+			const holidays = response.items.map(item => {
+				return {
+					startDate: item.start.date,
+					endDate: item.end.date,
+				};
+			});
+			
+			setHolidays(holidays);
+			
+		}).catch(error => {
+			console.log(error);
+		});
+	}, []);
+	
+	
+	const handlePlateNumber = (event) => {
+		const limit = 4;
+		setPlateNumber(event.target.value.slice(0, limit));
+	};
+	
+	const handlePlateDate = (event) => {
+		setPlateDate(event.target.value);
+	};
+	
+	const handlePlateTime = (event) => {
+		setPlateTime(event.target.value);
+	};
+	
+	// get last number from plate number
+	const getLastNumber = (plateNumber) => {
+		return plateNumber.slice(-1);
+	};
+	
+	// get day name from plate date
+	const getDayName = (plateDate) => {
+		const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+		const day = new Date(plateDate).getDay();
+		return days[day];
+	};
+	
+	return (
+		<div className="h-screen flex items-center justify-center bg-slate-900 ">
+			<div className="flex flex-col">
+				<h1 className="text-xl md:text-4xl text-white mb-6">Pico y Placa Predictor &#128678;</h1>
+				
+				
+				<label htmlFor="plateNumber" className="text-white mb-2 text-sm">Plate Number</label>
+				<input type="number"
+							 name="plateNumber"
+							 id="plateNumber"
+							 placeholder="Insert your plate number"
+							 className="bg-white border-0 p-2 outline-0 mb-7 rounded-2xl"
+							 value={plateNumber}
+							 onChange={handlePlateNumber}></input>
+				
+				<label htmlFor="picoYPlacaDate" className="text-white mb-2 text-sm">Date to Verify</label>
+				<input type="date"
+							 name="picoYPlacaDate"
+							 id="picoYPlacaDate"
+							 className="bg-white border-0 p-2 outline-0 mb-7 rounded-2xl" onChange={handlePlateDate}></input>
+				
+				<label htmlFor="picoYPlacaTime" className="text-white mb-2 text-sm">Verify Restriction Time</label>
+				<input type="time"
+							 name="picoYPlacaTime"
+							 id="picoYPlacaTime"
+							 className="bg-white border-0 p-2 outline-0 mb-7 rounded-2xl" onChange={handlePlateTime}></input>
+			
+			</div>
+		</div>);
 };
 
 export default App;
