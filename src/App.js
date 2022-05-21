7bee94ef6b994e629672cc16569bcf9aa2cd248e
David Paucar <soydavidpaucar@gmail.com>
2022-05-21 17:31:34 -0500

    :zap: fetch information from google calendar api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from 'react';
+
+const apiKey = process.env.REACT_APP_APIKEY;
+
+// get data from google calendar api
+const handleFetchHolidays = async () => {
+	const endpoint = `https://www.googleapis.com/calendar/v3/calendars/en.ec%23holiday%40group.v.calendar.google.com/events?key=${apiKey}`;
+	const response = await fetch(endpoint);
+	return await response.json();
+};
+
 const App = () => {
-	return <div>Healthcheck</div>;
+
 };
 
 export default App;
